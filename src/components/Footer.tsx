e3dea6de7197673adfc5b8284e1b9d7c34434adf
Olayinka Olayokun <214311533+olkatechnology@users.noreply.github.com>
2026-02-27 16:39:53 +0100

    Fixed footer social

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Linkedin, Twitter, Youtube } from "lucide-react";
+import { Linkedin, Twitter, Youtube, Facebook } from "lucide-react";
 import logo from "@/assets/zentroseo-logo.png";
 
 const footerLinks = {
