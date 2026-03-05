3ec2b4a815375419a451222bef150a38731c134d
Olayinka Olayokun <214311533+olkatechnology@users.noreply.github.com>
2026-03-05 11:52:19 +0100

    Correction

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./i18n";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
